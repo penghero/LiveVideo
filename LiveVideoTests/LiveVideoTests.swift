cf45084480f8d27f38efe0505366938c3e701652
peng <896733185@qq.com>
2025-02-17 09:51:21 +0800

    Initial Commit

---
+++ LiveVideoTests/LiveVideoTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiveVideoTests.swift
+//  LiveVideoTests
+//
+//  Created by chenpeng on 2025/2/17.
+//
+
+import Testing
+@testable import LiveVideo
+
+struct LiveVideoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
